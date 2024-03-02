e9f8f1a5de403f69cea72f3814e5071d420f28f7
Guilherme Fernandes de Souza <104801386+GuiSpky@users.noreply.github.com>
2024-03-02 08:50:06 -0300

    Add files via upload

---
+++ aula18.py
@@ -0,0 +1,20 @@
+# if / elif .... / else
+# se / se não se / se não
+
+condi1 = False
+condi2 = False
+condi3 = False
+condi4 = False
+
+if condi1:
+    print('Condição 1')
+elif condi2:
+    print('Condição 2')
+elif condi3:
+    print('Condição 3')
+elif condi4:
+    print('Condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+print('Fora do if')
